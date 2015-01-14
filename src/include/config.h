5ecc18ee23bd1dd1e0f3b69ed993772a22b7d783
E. Mei <emei@airpost.net>
2015-01-14 11:51:44 -0600

    Cleaned up extraneous configuration elements
    Removed number of unused and irrelevant settings; commented
    current ones better and created sections.

--- src/include/config.h
+++ src/include/config.h
@@ -1,7 +1,11 @@
 /* FILE: config.h */
 
-/* announce deprecation of prefix? or be quiet... */
-// #define PREFIX_NEEDED 1
+//========================================
+// COMPILE CONFIGURATION
+//========================================
+
+/* adds a homing beacon */
+// #define HOMING_BEACON 1
 
 /* Defines the Default wind amount */
 #define WIND_DEFAULT 1800.0
@@ -12,24 +16,17 @@
 /* adds slowed walking during AFK */
 #define SLOW_WALK 1
 
-/* The predictive timer predicts when the next event will occur, and
-   tries to set the timer to match. */
-/* #define PREDICTIVE TIMER 1 */
-
-/* Manipulate Script running dynamically */
-/* #define WAKESCRIPT 1 */
-
 /* enable the inclusion of adult features such as stripping and slut doll in
    the key */
 #define ADULT_MODE 1
 
+/* enable the start up introduction/hypno text provided that the required
+   "Intro Text" notecard is also present */
+// #define INTRO_STARTUP 1
+
 /* enable a test mode with minimal code changes in which RLV support always
    fails effectively disable the listener for the check reply */
-/* #undef DEBUG_BADRLV */
-
-/* sets the target for debugging messages either DEBUG to send on debug
-   channel or OWNER to use ownersay */
-#define DEBUG_TARGET 1
+// #define DEBUG_BADRLV 1
 
 /* enable developer support features such as not using RLV locks which would
    prevent editing the scripts or notecards, collection and reporting of
@@ -46,13 +43,13 @@
 
 #ifdef DEVELOPER_MODE
 /* enable additional debugging messages up to specified threshold.
-   the debug level can be changed at the chat line */
+   the debug level can be changed at the chat line or in prefs */
 #define DEBUG_LEVEL 0
 #endif
 
-/* enable the start up introduction/hypno text provided that the required
-   "Intro Text" notecard is also present */
-/* #undef INTRO_STARTUP */
+//========================================
+// PACKAGE DATA
+//========================================
 
 /* Define to the address where bug reports for this package should be sent. */
 #define PACKAGE_BUGREPORT "https://github.com/emmamei/cdkey/issues"
